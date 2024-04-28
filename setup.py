26f5af706c1a4cb1e277297e73dba119fae643f0
Paweł Rajter <prajter00@gmail.com>
2024-04-29 01:23:14 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="NOKIA24",
+    author="Paweł Rajter",
+    author_email="pawel.rajter@hotmail.com",
+    packages=find_packages(),
+)
